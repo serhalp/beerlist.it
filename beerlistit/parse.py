3b88f25380ceac7f454cef16138e6d00b0877002
Philippe Serhal <philippe.serhal@gmail.com>
2014-08-02 17:38:36 -0400

    Add silly first prototype of parsing component.

---
+++ beerlistit/parse.py
@@ -0,0 +1,21 @@
+import re
+import requests
+import bs4
+
+known_beers = { 'highwaytothedankerzone': True, 'blindpig': True }
+
+def fetch_page(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.content
+    else:
+        return None
+
+def extract_beers(doc):
+    """Given an HTML document, return a list of putative beers found therein."""
+    beers = []
+    soup = bs4.BeautifulSoup(doc)
+    for s in soup.stripped_strings:
+        if re.sub('\W+', '', s).lower() in known_beers:
+            beers.append({ 'name': s })
+    return beers
---
+++ tests/test_parse.py
@@ -0,0 +1,31 @@
+import unittest
+from beerlistit.parse import *
+
+class ParseTestCase(unittest.TestCase):
+    """Tests for parse.py"""
+
+    def test_fetch_page(self):
+        """Does fetching a URL return the right document?"""
+        html = fetch_page('http://google.com')
+        self.assertIs(type(html), str)
+        self.assertTrue('Google' in html)
+
+    def test_fetch_page_fail(self):
+        """Does fetching a nonexistant URL return None?"""
+        html = fetch_page('http://google.com/jfnsdajnfnensensan')
+        self.assertIsNone(html)
+
+    def test_extract_beers(self):
+        """Are beers successfully extracted from an HTML document?"""
+        html = open('tests/example_menu_page.html', 'r').read()
+        beers = extract_beers(html)
+        self.assertIs(type(beers), list)
+        self.assertEquals(len(beers), 1)
+        self.assertEquals(len(beers[0]), 1)
+        self.assertEquals(beers[0]['name'], 'Blind Pig')
+
+    def test_extract_beers_fail(self):
+        """Are fake beers unsuccessfully extracted from an HTML document?"""
+        beers = extract_beers('<div>Fakebeer</div><span>Not a real IPA I swear</span>')
+        self.assertIs(type(beers), list)
+        self.assertEquals(len(beers), 0)
