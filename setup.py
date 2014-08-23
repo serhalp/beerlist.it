f0a8ef314a6ac289fd7daf1f96124c1f1dec06db
Philippe Serhal <philippe.serhal@gmail.com>
2014-08-23 11:44:01 -0400

    Fix setup.py.

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
-setup(name='beerlist.it',
+setup(name='beerlistit',
       version='0.1',
       description='Quickly get the BeerAdvocate ratings for all the beers listed on a page.',
       url='http://github.com/serhalp/beerlist.it',
       author='Philippe Serhal and Curtis Heberle',
       author_email='philippe.serhal@gmail.com',
       license='GPLv2',
-      packages=['beerlist.it'],
+      packages=['beerlistit'],
       install_requires=[
-          'BeautifulSoup',
+          'BeautifulSoup4',
           'requests'
       ],
       zip_safe=True)
